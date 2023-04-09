9cda50d97524d20dd144b7d9542e3e208ac55aa2
MarlonHD <marlonsilva840@gmail.com>
2023-04-09 17:38:27 -0300

    receita1 - passo 7

--- receita1.dart
+++ receita1.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized(); //ativar importação do google_fonts
   MaterialApp app = MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.cyan),
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        fontFamily: GoogleFonts.oswald().fontFamily,
+      ),
       home: Scaffold(
         appBar: AppBar(title: Text("Meu app")),
         body: Center(
