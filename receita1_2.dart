d42ca40128931b08dd45734bc3e49cf17e4cd8e8
MarlonHD <marlonsilva840@gmail.com>
2023-04-09 17:48:09 -0300

    receita1 - passo 10

---
+++ receita1_2.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); //ativar importação do google_fonts
+  MaterialApp app = MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+        fontFamily: GoogleFonts.oswald().fontFamily,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Meu app")),
+        body: DataTable(
+            columns: const <DataColumn>[
+              DataColumn(label: Text("Nome")),
+              DataColumn(label: Text("Estilo")),
+              DataColumn(label: Text("IBU")),
+            ],
+            rows: const <DataRow>[
+              DataRow(cells: <DataCell>[
+                DataCell(Text("La Fin Du Monde")),
+                DataCell(Text("Bock")),
+                DataCell(Text("65")),              
+              ]),
+              DataRow(cells: <DataCell>[
+                DataCell(Text("Sapporo Premium")),
+                DataCell(Text("Sour Ale")),
+                DataCell(Text("54")),
+              ]),
+              DataRow(cells: <DataCell>[
+                DataCell(Text("Duvel")),
+                DataCell(Text("Pilsner")),
+                DataCell(Text("82")),
+              ])
+            ]),
+        bottomNavigationBar: Row(
+          children: [
+            Expanded(
+              child: ElevatedButton(child: Text("Botão 1"), onPressed: () => print("Botao 1"))),
+            Expanded(
+              child: ElevatedButton(child: Text("Botão 2"), onPressed: () => print("Botao 2"))),
+            Expanded(
+              child: IconButton(icon: const Icon(Icons.bluetooth), iconSize: 30, onPressed: () => print("bluetooth on")))
+          ]),
+      ));
+  
+  runApp(app);
+}
